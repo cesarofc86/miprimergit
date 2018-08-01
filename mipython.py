3eda5ce7fc66b55ae1c8f5ed75a01cbaa9bd9202
cesar_octavio <cesarofc1986@gmail.com>
2018-08-01 14:45:14 -0600

    este es el primer commit

---
+++ mipython.py
@@ -0,0 +1,21 @@
+def saludos():
+    print("hola")
+
+saludos()
+
+
+class automovil():
+    def __init__(self,marca,parado):
+        self.marca=marca
+        self.parado=parado
+
+    def quemarca(self):
+        return self.marca
+
+    def avanza(self):
+        if self.parado=="si":
+            return "esta parado"
+
+micarro=automovil("jetta","si")
+print(micarro.quemarca())
+print(micarro.avanza())
